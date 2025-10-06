c0a10b7dd47fad25e73625b19fc4f0b29ba424ff
Keerthi2311 <keerthihegde516@gmail.com>
2025-10-06 11:09:34 +0530

    02:package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,17 @@
-import { Button } from "@workspace/ui/components/button" 
-import { add } from "@workspace/math/add"
+"use client";
 
-export default function Page() {
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+
+export default function Page() { 
+  const users = useQuery(api.user.getMany);
+  
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2,2)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/dev</p>
+      <div className="max-w-sm w-full mx-auto">
+      {JSON.stringify(users,null,2)}
+    </div>
     </div>
   )
 }
